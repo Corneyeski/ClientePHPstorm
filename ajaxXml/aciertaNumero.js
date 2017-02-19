49dd109db03908eb6d59d348dce7c973bc934363
Alan <alanvallve@gmail.com>
2017-02-19 11:15:08 +0100

    Practica ajax

---
+++ ajaxXml/aciertaNumero.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("#inicioXML").click(numeroAleatorio);
+});
+
+function numeroAleatorio(){
+    var xmlHttp = new XMLHttpRequest();
+
+    // url destino + parámetros
+    xmlHttp.open("GET", "aciertaNumeroXML.php?inicio=si", true);
+    xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // qué hacer con la respuesta, quién recibe
+    xmlHttp.onreadystatechange = function(){
+        // state 4 = completado
+        if(xmlHttp.readyState === 4){
+            console.log(xmlHttp);
+            recibirNumero(xmlHttp);
+        }
+    }
+    // enviar la petición + los datos que queramos
+    xmlHttp.send(null);
+}
+function recibirNumero(xmlHttp){
+    if(xmlHttp.status === 200){
+        var respuesta = xmlHttp.responseXML;
+
+        var numAleatorio = respuesta.getElementsByTagName("inicio")[0].textContent;
+        console.log(numAleatorio);
+        document.getElementById("mensaje").innerHTML = "Se ha generado un nuevo número!!";
+    }
+}
